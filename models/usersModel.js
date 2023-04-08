6c8c7d051f9cfd03a8f8c1818d67faec0c9cb46e
unknown <kaah6978@gmail.com>
2023-04-08 17:53:39 +0300

    test

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,15 +4,12 @@ const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      require: true,
     },
     email: {
       type: String,
-      require: true,
     },
     password: {
       type: String,
-      require: true,
     },
     role: {
       type: Number,
