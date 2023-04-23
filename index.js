eae004dba500b221e3ba371032ae70484834bcd0
unknown <kaah6978@gmail.com>
2023-04-23 16:35:50 +0300

    test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import dotenv from 'dotenv'
+
 
 
 dotenv.config()
@@ -13,6 +15,7 @@ const app = express();
 
 app.use('/api/users', userRoutes)
 app.use('/api/projects', projectRoutes) 
+app.use('/api/upload', uploadRoutes)
 
 
 const port = 5000;
