93e4ca583da4ae0ad563019c1720f1a62820d053
unknown <kaah6978@gmail.com>
2023-04-28 17:25:47 +0300

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBuyerUser, createUser, deletUser, getAllUser, getUserProfileById, login, updateUser, updateUserRole } from '../controllers/userController.js'
+import { addToRequest, addToWishlist, createBuyerUser, createUser, deletUser, getAllUser, getUserProfileById, login, updateUser, updateUserRole } from '../controllers/userController.js'
 import { admin, protect } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
@@ -10,7 +10,8 @@ router.route('/:id').put(updateUser).delete(deletUser)
 router.route('/login').post(login)
 router.route('/profile/:id').post(getUserProfileById)
 router.route('/role/:id').put(protect, admin, updateUserRole)
-
+router.route('/add-to-req').post(protect, addToRequest)
+router.route('/add-to-wishlist').post(protect, addToWishlist)
 
 
 export default router;
