756e9750cd12261dd858711c1da182e244c3425a
unknown <kaah6978@gmail.com>
2023-06-19 11:19:48 +0300

    test

---
+++ seeder.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+import connectDB from './config/db.js'
+import Project from './models/projectModel.js'
+
+
+
+dotenv.config()
+
+connectDB()
+
+const importData = async () => {
+    try {
+        await Project.deleteMany()
+        // await Product.deleteMany()
+        // await User.deleteMany()
+        // await Slide.deleteMany()
+
+        // const createUsers = await User.insertMany(users)
+
+        // const adminUser = createUsers[0]._id
+        // const sampleProducts = products.map(product => {
+        //     return {...product, user: adminUser}
+        // })
+
+        // await Product.insertMany(sampleProducts)
+
+        // await Slide.insertMany(slides)
+        //  await Promotions.insertMany(promotions)
+       // await Testimonials.insertMany(testimonials)
+
+        // const adminUser2 = createUsers[0]._id
+        // const sampleSlides = products.map(product => {
+        //     return {...product, user: adminUser2}
+        // })
+
+        // await Slide.insertMany(sampleSlides)
+        console.log('Data Imported! '.green.inverse)
+        process.exit()
+ 
+    } catch (error) {
+        console.error(`${error}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+
+const destroyData = async () => {
+    try {
+        await Oder.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        
+        console.log('Data Destroyed! '.red.inverse)
+        process.exit()
+
+    } catch (error) {
+        console.error(`${error}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+if (process.argv[2] == '-d') {
+    destroyData()
+}
+else {
+    importData()
+}
